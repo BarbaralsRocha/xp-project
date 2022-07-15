b205f119e5f8cc7a76c68f3b15ed31c3f2399a07
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-15 18:14:27 -0300

    feat: middleware authentication token

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,13 @@
+const { authenticateToken } = require('../utils/JWTToken');
+
+const authenticationMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+    
+    const payload = await authenticateToken(token);
+
+    res.locals.payload = payload;
+
+    next();
+};
+
+module.exports = authenticationMiddleware; 
