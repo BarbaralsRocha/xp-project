fa042109c5f182970a29b83d027ac792750d43ea
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-17 18:44:26 -0300

    fix: buy controller requisition

--- src/controller/buy.controller.js
+++ src/controller/buy.controller.js
@@ -2,12 +2,13 @@ const buyService = require('../service/buy.service');
 
 const buy = async (req, res) => {
     const { id } = res.locals.payload
-    const { codAtivo, qtdeAtivos } = req.body;
+    const { codAtivo, qtdeAtivos, conta } = req.body;
     const type = 'buy'
     try{
-        await buyService(id, codAtivo, type, qtdeAtivos);
+        await buyService(id, codAtivo, type, qtdeAtivos, conta);
         return res.status(200).json({ codCliente: id, codAtivo, qtdeAtivos });
     } catch (e){
+        console.log(e)
         return res.status(401).json({ message: 'Quantidade de ativos excedido' })
     }
 };
