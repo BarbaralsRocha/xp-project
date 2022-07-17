046c37ef452b8391ae94bf0898d8917e74012f42
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-17 18:36:33 -0300

    feat: user service

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -19,9 +19,60 @@ const createUser = async ({
 const getUsers = () => User.findAll({
     attributes: { exclude: ['password'] } });
 
+const updateWalletUser = async(amount, balance, id, type) =>{
+    let rest;
+    if(type === 'buy'){
+        rest = balance - amount;
+    }
+    rest = balance + amount;
+    User.update(
+        {
+        balance: rest,
+        },
+        {
+        where: { id }
+})
+}
+
+const amount = async (valor, codCliente, type) => {
+    const findUser = await User.findByPk(codCliente);
+    if (!findUser) {
+        throw new Error(JSON.stringify({ status: 401, message: 'Usuário nao encontrado' }));
+    }
+    const { balance } = findUser.dataValues
+    let sum;
+    if(type === 'withdraw'){
+        sum = balance - valor
+        if(sum < 0) {
+            throw new Error(JSON.stringify({ status: 401, message: 'Saldo indisponível' }));
+        }
+    }
+    sum = balance + valor;
+    User.update(
+        {
+        balance: sum,
+        },
+        {
+        where: { id: codCliente }
+})
+    return true;
+}
+
+const getAmount = async (codCliente) => {
+    const findUser = await User.findByPk(codCliente);
+    if (!findUser) {
+        throw new Error(JSON.stringify({ status: 401, message: 'Usuário nao encontrado' }));
+    }
+    const { balance } = findUser.dataValues
+    return balance
+}
+
 module.exports = {
     createUser,
-    getUsers
+    getUsers,
+    updateWalletUser,
+    amount,
+    getAmount
 }; 
 
     
