1a86c0c72c584640c0452b7e1f23a6f2d5b8ec10
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-17 18:32:28 -0300

    fix: change port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const app = require('./app');
 
 // eslint-disable-next-line no-undef
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 3006;
 
 app.listen(port, () => console.log('ouvindo porta', port));
