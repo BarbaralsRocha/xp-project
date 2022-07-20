02df5b337bcffcaffa93c774a00e898c8deaedfb
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-19 22:19:28 -0300

    delete file

--- src/routers/actives.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const activesController = require('../controller/actives.controller');
-const authentication = require('../middlewares/authentication');
-
-const routes = express.Router();
-
-routes.get('/:id', authentication, activesController);
-
-module.exports = routes;
