411fc8502b6ab90a3532e5c01ef8226c0af932c6
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-19 22:16:37 -0300

    feat: catch errors user controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,15 +1,16 @@
 const userService = require('../service/user.service');
 const authentication = require('../utils/generateToken');
 
-const newUser = async (req, res) => {
+const newUser = async (req, res, next) => {
     try {
         await userService.createUser(req.body);
+        console.log('user')
         const token = await authentication(req.body);
         const { name, email, balance } = req.body
         return res.status(201).json({name, email, balance, token});
     } catch (e) {
-        console.log(e.message)
-        return res.status(409).json({ message: 'User already registered' });
+        console.log(e);
+        next(e);
     }
 };
 
