c65cdd93f5b6f46ba70f571715aae1c65cec5b85
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-19 22:15:51 -0300

    fix: catch errors buy controller

--- src/controller/buy.controller.js
+++ src/controller/buy.controller.js
@@ -1,15 +1,16 @@
 const buyService = require('../service/buy.service');
+require('express-async-errors');
 
-const buy = async (req, res) => {
+const buy = async (req, res,next) => {
     const { id } = res.locals.payload
     const { codAtivo, qtdeAtivos, conta } = req.body;
     const type = 'buy'
     try{
         await buyService(id, codAtivo, type, qtdeAtivos, conta);
-        return res.status(200).json({ codCliente: id, codAtivo, qtdeAtivos });
+        return res.status(201).json({ codCliente: id, codAtivo, qtdeAtivos });
     } catch (e){
-        console.log(e)
-        return res.status(401).json({ message: 'Quantidade de ativos excedido' })
+        console.log(e);
+        next(e);
     }
 };
 
