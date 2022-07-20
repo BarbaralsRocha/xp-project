377954e3ef50b5155b5a22b69c1b0867468d1ca1
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-20 19:02:16 -0300

    fix: number price assets

--- src/service/active.service.js
+++ src/service/active.service.js
@@ -3,34 +3,34 @@ const getClientsById = require('../db/model/assetsByClient.model')
 
 const updateQuantityAssets = (restQuantity, codAtivo) => Active.update(
     {
-    quantity: restQuantity
+        quantity: restQuantity
     },
     {
-    where: { id: codAtivo}
-});
-
-const getAssetsById = async (id) => {
-    const assets = await Active.findByPk(id)
-    console.log(assets)
-    if(!assets) throw new Error(JSON.stringify({ status: 409, message: 'Ativo não encontrado' }))
-    const { quantity, price } = assets.dataValues;
-    return {
-        codAtivo: id,
-        qtdeAtivos: quantity,
-        valor: price,
-    }
-};
-
-const assetsClientsById = async (id) => {
-    const type = 'buy'
-    const result = await getClientsById(id, type)
-    if(result.length === 0) throw new Error(JSON.stringify({ status: 401, message: 'Usuário não encontrado' }))
-    const resultado = await Promise.all(result.map((el) => {
+        where: { id: codAtivo}
+    });
+    
+    const getAssetsById = async (id) => {
+        const assets = await Active.findByPk(id)
+        console.log(assets)
+        if(!assets) throw new Error(JSON.stringify({ status: 409, message: 'Ativo não encontrado' }))
+        const { quantity, price } = assets.dataValues;
+        return {
+            codAtivo: id,
+            qtdeAtivos: quantity,
+            valor: Number(price),
+        }
+    };
+    
+    const assetsClientsById = async (id) => {
+        const type = 'buy'
+        const result = await getClientsById(id, type)
+        if(result.length === 0) throw new Error(JSON.stringify({ status: 401, message: 'Usuário não encontrado' }))
+        const resultado = await Promise.all(result.map((el) => {
         return {
         codCliente: el.userId,
         codAtivo: el.assetsId,
         qtdeAtivos: el.quantity,
-        valor: el.price,
+        valor: Number(el.price),
         }
     }))
     return resultado
