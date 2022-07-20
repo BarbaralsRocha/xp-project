e919eefedf310dc59be98aeacf3874cfd22cd54a
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-19 22:16:22 -0300

    feat: catch errors sale controller

--- src/controller/sale.controller.js
+++ src/controller/sale.controller.js
@@ -1,15 +1,17 @@
 const saleService = require('../service/sale.service');
+require('express-async-errors');
 
-const sale = async (req, res) => {
+const sale = async (req, res, next) => {
     const { id } = res.locals.payload
     const { codAtivo, qtdeAtivos, conta } = req.body;
     const type = 'sale'
     try{
         await saleService(id, codAtivo, type, qtdeAtivos, conta);
-        return res.status(200).json({ codCliente: id, codAtivo, qtdeAtivos });
+        return res.status(201).json({ codCliente: id, codAtivo, qtdeAtivos });
     } catch (e){
-        console.log(e)
-        return res.status(401).json({ message: 'Quantidade de ativos excedido' })
+        console.log(e);
+        next(e);
+        return res.status(401).json({ message: 'Qtde de ativos excedido' });
     }
 };
 
