6aeac6630eb07d6612c21f465e5b8863df55eb9e
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-19 22:15:15 -0300

    fix: catch errors

--- src/controller/amount.controller.js
+++ src/controller/amount.controller.js
@@ -1,37 +1,34 @@
 const userService = require('../service/user.service');
 
-const deposit = async (req, res) => {
+const deposit = async (req, res, next) => {
     const { valor, codCliente } = req.body
     try{
-        const result = await userService.amount(valor, codCliente, 'deposit');
-        if(result) return res.status(200).json(req.body);
-        return res.status(401).json({ message: 'Usuário não encontrado' });
+        await userService.amount(valor, codCliente, 'deposit');
+        return res.status(201).json(req.body);
     } catch (e){
-        console.log(e)
-        return res.status(401).json({ message: 'Erro inesperado' })
+        console.log(e);
+        next(e);
     }
 };
 
-const withdraw = async (req, res) => {
+const withdraw = async (req, res, next) => {
     const { valor, codCliente } = req.body
     try{
-        const result = await userService.amount(valor, codCliente, 'withdraw');
-        if(result) return res.status(200).json(req.body);
-        return res.status(401).json({ message: 'Usuário não encontrado' });
+        await userService.amount(valor, codCliente, 'withdraw');
+        return res.status(200).json(req.body);
     } catch (e){
-        console.log(e)
-        return res.status(401).json({ message: 'Erro inesperado' })
+        console.log(e);
+        next(e);
     }
 };
 
-const amount = async (req, res) => {
-    console.log('entrou')
+const amount = async (req, res, next) => {
     try{
         const result = await userService.getAmount(req.params.id);
         return res.status(200).json({ codCliente: req.params.id, saldo: result});
     } catch (e){
-        console.log(e)
-        return res.status(401).json({ message: 'Erro inesperado' })
+        console.log(e);
+        next(e);
     }
 };
 
