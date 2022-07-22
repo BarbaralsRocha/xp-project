ec00172af25b883288890948857af8e170d3c6f3
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-22 13:55:16 -0300

    refactor: change logic to do swagger token

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,8 +2,17 @@ const { authenticateToken } = require('../utils/JWTToken');
 
 const authenticationMiddleware = async (req, res, next) => {
     const token = req.headers.authorization;
-    
-    const payload = await authenticateToken(token);
+
+    const [, auth] = token.split(' ');
+    // Lógica para funcionar tanto no swagger, quanto na requisição local
+    let payload;
+    if(token && !auth) {
+        payload = await authenticateToken(token);
+    } else if (!token && auth){
+        payload = await authenticateToken(auth);
+    } else if (token && auth){
+        payload = await authenticateToken(auth);
+    }
 
     res.locals.payload = payload;
 
