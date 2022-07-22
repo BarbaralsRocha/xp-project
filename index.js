1f191098562f6d2da888c0fc017af5561a31a059
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-22 13:57:26 -0300

    feat: swagger server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerConfig = require('./src/utils/swagger.config');
 const app = require('./app');
 
 // eslint-disable-next-line no-undef
 const port = process.env.API_PORT || 3006;
 
+const swaggerDocs = swaggerJsdoc(swaggerConfig)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.listen(port, () => console.log('ouvindo porta', port));
