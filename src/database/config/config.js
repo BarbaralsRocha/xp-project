b7cd7c348c2cee3c68fc05e004ec83a0caeb1feb
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-22 18:27:42 -0300

    fix: change config sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -18,10 +18,6 @@ module.exports = {
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DB_NAME,
-    "host": process.env.MYSQL_HOST,
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
