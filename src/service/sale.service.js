79d13192487fc2e93c65c8acc43d08b8d76e4e23
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-22 13:46:13 -0300

    refactor: sale service functions

--- src/service/sale.service.js
+++ src/service/sale.service.js
@@ -5,26 +5,20 @@ const activeService = require('./active.service');
 
 const sale = async (id, codAtivo, type, qtdeAtivos, conta) => {
     const findAsset = await Active.findByPk(codAtivo);
-    const { quantity, price } = findAsset.dataValues;
-
+    const { quantity, price } = Object.values(findAsset)[0];
     const findUser = await User.findByPk(id);
-    const { balance, account } = findUser.dataValues;
-
+    // console.log(Object.assign(findUser))
+    const { balance, account } = Object.assign(findUser);
+    console.log({account, conta})
     if(account !== conta){
         throw new Error(JSON.stringify({ status: 401, message: 'Conta inválida' }));
     }
     
-    // checkQuantityAssets {
-    const result = await Transaction.findAll({
-        where: { userId: id }
-    }
-            );
-        const getQuantity = await Promise.all(result.map((el) => el.quantity))
-        const amountAssets = getQuantity.reduce((acc,cv) => acc + cv ,0)
+    const amountAssets = await checkQuantityAssets(id)
+
         if(qtdeAtivos > amountAssets) {
             throw new Error(JSON.stringify({ status: 401, message: 'Qtde de ativos excedido' }))
         }
-    // }
 
     userService.updateWalletUser(qtdeAtivos * price, balance, id, 'sale')
     activeService.updateQuantityAssets((quantity + qtdeAtivos), codAtivo)
@@ -37,9 +31,14 @@ const sale = async (id, codAtivo, type, qtdeAtivos, conta) => {
 })
 };
 
-// const checkQuantityAssets = async (qtdeAtivos, id) => {
+const checkQuantityAssets = async (id) => {
+    const result = await Transaction.findAll({
+        where: { userId: id }
+    }
+            );
+    const getQuantity = await Promise.all(result.map((el) => el.quantity))
+    const amountAssets = getQuantity.reduce((acc,cv) => acc + cv ,0)
+    return amountAssets;
+}
 
-// }
-
-
-module.exports = sale
+module.exports = { sale, checkQuantityAssets }
