1b64551792f7a265d258925073d7f292584d0961
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-07-24 11:19:19 -0300

    refactor: sale controller

--- src/controller/sale.controller.js
+++ src/controller/sale.controller.js
@@ -6,7 +6,7 @@ const sale = async (req, res, next) => {
     const { codAtivo, qtdeAtivos, conta } = req.body;
     const type = 'sale'
     try{
-        await saleService(id, codAtivo, type, qtdeAtivos, conta);
+        await saleService.sale(id, codAtivo, type, qtdeAtivos, conta);
         return res.status(201).json({ codCliente: id, codAtivo, qtdeAtivos });
     } catch (e){
         console.log(e);
